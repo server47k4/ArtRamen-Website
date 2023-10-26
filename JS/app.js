dd74e58490b6337077529b7868a2028f917c0bf2
JimK <132590388+web-dev-pr0@users.noreply.github.com>
2023-10-26 10:05:55 +0300

    Completed the Slider

--- JS/app.js
+++ JS/app.js
@@ -3,21 +3,29 @@ const leftArrow = document.querySelector(".left-arrow");
 const rightArrow = document.querySelector(".right-arrow");
 const dot = document.querySelector(".dot");
 
-
+// Reviews Slider Small Screen Sizes
+let counter = 0;
 rightArrow.addEventListener("click", () => {
-  const activeReview = document.querySelector(".active-review-container");
-  const activeDot = document.querySelector(".active-dot");
+  if (counter < 3) {
+    counter++;
 
-  if (activeReview && activeDot) {
+    // Gets the elements that have the active class
+    const activeReview = document.querySelector(".active-review-container");
+    const activeDot = document.querySelector(".active-dot");
+
+    // Removes the active class from the elements that have it
     activeDot.classList.remove("active-dot");
     activeReview.classList.remove("active-review-container");
 
-      const nextDot = activeDot.nextElementSibling;
-      const nextReview = activeReview.nextElementSibling;
+    // Gets the next sibling of the element that has the active class
+    const nextDot = activeDot.nextElementSibling;
+    const nextReview = activeReview.nextElementSibling;
 
+    // Adds the active class to the next sibling
     nextDot.classList.add("active-dot");
     nextReview.classList.add("active-review-container");
 
+    // Removes the active class from the invisible container and adds it to the previous sibling
     if (nextReview.classList.contains("invisible-container")) {
       nextReview.classList.remove("active-review-container");
       nextReview.previousElementSibling.classList.add(
@@ -28,17 +36,23 @@ rightArrow.addEventListener("click", () => {
 });
 
 leftArrow.addEventListener("click", () => {
-  const activeDot = document.querySelector(".active-dot");
-  const activeReview = document.querySelector(".active-review-container");
+  if (counter > 0) {
+    counter--;
+    
+    // Gets the elements that have the active class
+    const activeDot = document.querySelector(".active-dot");
+    const activeReview = document.querySelector(".active-review-container");
 
-  if (activeReview && activeDot) {
+    // Removes the active class from the elements that have it
     activeDot.classList.remove("active-dot");
     activeReview.classList.remove("active-review-container");
 
-    const nextDot = activeDot.previousElementSibling;
-    const nextReview = activeReview.previousElementSibling;
+    // Gets the previous sibling of the element that has the active class
+    const previousDot = activeDot.previousElementSibling;
+    const previousReview = activeReview.previousElementSibling;
 
-    nextDot.classList.add("active-dot");
-    nextReview.classList.add("active-review-container");
+    // Adds the active class to the previous sibling
+    previousDot.classList.add("active-dot");
+    previousReview.classList.add("active-review-container");
   }
 });
